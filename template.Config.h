3861daa6ada13728e3c4505b45bed70ca3f07e1c
Rubén Cacio <rcaciocamacho@gmail.com>
2024-05-10 14:10:40 +0200

    Se han sacado las variables de configuración a un fichero externo.

---
+++ template.Config.h
@@ -0,0 +1,13 @@
+// Config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* ssid = "TU_SSID";
+const char* password = "TU_PASSWORD";
+const char* gotifyURL = "https://gotify.example.com/message?token=YOUR_APP_TOKEN";
+const char* notifyMeAPIKey = "YOUR_NOTIFY_ME_API_KEY";
+const char* notifyMeURL = "https://api.notifymyecho.com/v1/NotifyMe";
+const char* serverURL = "http://yourserver.com/request_chatgpt";
+const char* heartbeatURL = "http://yourserver.com/heartbeat";
+
+#endif
